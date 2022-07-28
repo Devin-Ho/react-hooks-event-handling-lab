7f8acd287e515d7cc4006462b27d7dd24070860d
Devin Ho <djxho36@gmail.com>
2022-07-28 09:29:31 -0400

    commit tree 2d595fe96acf3cefccf8cca7c89d3a2b8f50b346

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+
+function Blur () {
+  console.log("Hey! Eyes on me!")
+}
+
+function Focus () {
+  console.log("Good!")
+}
+
+function EyesOnMe () {
+  return (
+    <button onBlur = {Blur} onFocus = {Focus}>Eyes on me</button>
+  );
+}
+
+export default EyesOnMe;
